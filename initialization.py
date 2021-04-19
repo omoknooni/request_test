6633e223790acc09c0ee4afea3ce2526a2c13fa3
onaeonae1 <onaeonae2@gmail.com>
2021-04-19 14:57:53 +0900

    changed main arguments

--- initialization.py
+++ initialization.py
@@ -1,28 +1,63 @@
 import typing
+import os
+
+
+def read_file(workfolder, filename):
+    token_list = list()
+    token_path = os.path.join(workfolder, filename)
+    print("Reading file from : ", token_path)
+    try:
+        with open(token_path, "r") as word_txt:
+            for item in word_txt.readlines():
+                token_list.append(item.rstrip())
+    except Exception as e:
+        # other error handling for file I/O
+        print(e)
+    return token_list
 
 
 def init_setting(
-    url: str, mode: str, port: int, max_depth: int, timeout: float, word_path: str
+    url: str,
+    port: int,
+    max_depth: int,
+    timeout: float,
+    delay: float,
+    random_agent: bool,
+    request_delay: bool,
+    config_folder: str,
+    report_folder: str,
+    word_path: str,
+    dir_path: str,
+    fnm_path: str,
+    ext_path: str,
 ) -> list:
     # port, max_depth, timeout, word_path
     url = url if url.startswith("http://") else "http://" + url
     word_list = list()
+    dir_list = list()
+    fnm_list = list()
+    ext_list = list()
+
     url_set = dict()
-    try:
-        with open(word_path, "r") as word_txt:
-            for item in word_txt.readlines():
-                word_list.append(item.rstrip())
-    except Exception as e:
-        print("Reading Error : Using Default Words")
-        print(e)
-        word_list.clear()
-        word_list = [
-            "",
-            "src/",
-            "bootstrap/",
-            "boards/",
-            "css/",
-            "js/",
-            "admin/",
-        ]
-    return [url, word_list, max_depth, timeout, url_set, port]
+
+    word_list = read_file(config_folder, word_path)
+    dir_list = read_file(config_folder, dir_path)
+    fnm_list = read_file(config_folder, fnm_path)
+    ext_list = read_file(config_folder, ext_path)
+
+    # return REQUIRED DATA ONLY (which required for afterwards)
+    return [
+        url,
+        port,
+        max_depth,
+        timeout,
+        delay,
+        random_agent,
+        request_delay,
+        report_folder,
+        word_list,
+        dir_list,
+        fnm_list,
+        ext_list,
+        url_set,
+    ]
--- main.py
+++ main.py
@@ -9,36 +9,103 @@ import report
 
 
 def main() -> list:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("url", help="Enter URL or Domain to Inspect")
-    parser.add_argument(
-        "-mode",
-        default="default",
-        help="Enter custom to configure port, max_depth, etc..",
+    parser = argparse.ArgumentParser(
+        description="Directory checking tool as a web scanning, random-agent option and random-delay option is recommanded"
     )
+    parser.add_argument("url", help="Enter URL or Domain to Inspect")
     parser.add_argument("-port", default=80, help="Enter PORT to inspect e.g) 80, 8080")
     parser.add_argument("-max_depth", default=3, help="Enter max-depth to use")
     parser.add_argument(
         "-timeout", default=0.5, help="Enter maximum timeout for each connection"
     )
     parser.add_argument(
-        "-word_path",
-        default="word_list",
-        help="Enter your word_list.txt path.",
+        "-d",
+        help="Add delay between requests, default value is 1",
+        dest="delay",
+        type=float,
+        default=1,
+    )
+    parser.add_argument(
+        "-ra",
+        help="Enable RandomAgent mode",
+        dest="random_agent",
+        required=False,
+        default=True,
+    )
+    parser.add_argument(
+        "-rt",
+        help="Enable Random request dealy mode",
+        dest="request_delay",
+        required=False,
+        default=True,
+    )
+    parser.add_argument(
+        "-report",
+        help="A specific Folder to Store JSON reports",
+        dest="report_folder",
+        type=str,
+        required=False,
+        default="reports",
+    )
+    parser.add_argument(
+        "-config",
+        help="A Specific Folder to Load your config files : name_book, word_path etc..",
+        dest="config_folder",
+        type=str,
+        required=False,
+        default="configs",
+    )
+    parser.add_argument(
+        "-w",
+        dest="word_path",
+        default="word_list.txt",
+        type=str,
+        required=False,
+        help="A specific path to word_list file",
+    )
+    parser.add_argument(
+        "-l",
+        help="A specific path to dir_list file",
+        dest="dir_path",
+        type=str,
+        required=False,
+        default="dir_book.txt",
+    )
+    parser.add_argument(
+        "-f",
+        help="A specific path to filename_list file",
+        dest="fnm_path",
+        type=str,
+        required=False,
+        default="name_book.txt",
+    )
+    parser.add_argument(
+        "-e",
+        help="A specific path to extenion_list file",
+        dest="ext_path",
+        type=str,
+        required=False,
+        default="extension_book.txt",
     )
     args = parser.parse_args()
     return args
 
 
 def get_settings(args: list) -> list:
-    args.word_path = "./config/" + args.word_path + ".txt"
     conf = initialization.init_setting(
         url=args.url,
-        mode=args.mode,
         port=args.port,
         max_depth=args.max_depth,
         timeout=args.timeout,
+        delay=args.delay,
+        random_agent=args.random_agent,
+        request_delay=args.request_delay,
+        config_folder=args.config_folder,
+        report_folder=args.report_folder,
         word_path=args.word_path,
+        dir_path=args.dir_path,
+        fnm_path=args.fnm_path,
+        ext_path=args.ext_path,
     )
     return conf
 
@@ -53,6 +120,7 @@ if __name__ == "__main__":
     print("[2] : Loading Settings from parsed Arguments")
 
     conf = get_settings(args)
+    print(conf)
     url = conf[0]
 
     print("[2] : Done")
@@ -63,7 +131,7 @@ if __name__ == "__main__":
     print("[3] : Done")
     print("[4] : Making Reports")
 
+    # make reports
     report.make_report(initial_result, "initial")
     report.make_report(final_result, "final")
     print("[4] : All DONE! Check ./report/ ")
-    # make reports
